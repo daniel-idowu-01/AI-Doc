7ed62b8032191fafbd5d37fda1263ea75bb82cce
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-15 22:29:49 +0100

    Update dependencies in yarn.lock and package-lock.json

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,164 +1,166 @@
 import React, { useState } from "react";
-import { Logo,  Google } from '../assets/images';
-import AIbot from '../components/AIbot';
+import { Logo, Google } from "../assets/images";
+import AIbot from "../components/AIbot";
 import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
+  const [formData, setFormData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-    const [formData, setFormData] = useState({})
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState(null)
-    const navigate = useNavigate();
-
-    // to update user inputs
+  // to update user inputs
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   // to submit form data
   const handleSubmit = async (e) => {
-      e.preventDefault();
+    e.preventDefault();
 
     try {
-      setIsLoading(true)
-      const response = await fetch('https://ai-doc-7h0i.onrender.com/api/v1/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData)
-      });
+      setIsLoading(true);
+      const response = await fetch(
+        "https://ai-doc-7h0i.onrender.com/api/v1/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       const data = await response.json();
-      if(data.success === false ) {
+      if (data.success === false) {
         setIsLoading(false);
         setError(data.message);
       }
-        setIsLoading(false);
-        if (data.accessToken) {
-            navigate('/home')
-        } else {
-            setError('Email or password not correct!')
-        }
-        localStorage.setItem('accessToken', JSON.stringify(data.accessToken))
-        localStorage.setItem('userDetails', JSON.stringify(data.user))
+      setIsLoading(false);
+      if (data.accessToken) {
+        navigate("/home");
+      } else {
+        setError("Email or password not correct!");
+      }
+      localStorage.setItem("accessToken", JSON.stringify(data.accessToken));
+      localStorage.setItem("userDetails", JSON.stringify(data.user));
     } catch (error) {
       setIsLoading(false);
       setError(error.message);
-    } 
     }
-    
-    const handleForgotPassword = async () => {
-        const response = await fetch('https://ai-doc-7h0i.onrender.com/api/v1/auth/forgot-password', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData.email)
-      });
-    }
+  };
 
-    return (
-        <section className="clash-variable flex gap-8 justify-items-center items-center overflow-y-hidden">
-            <article className="hidden md:block left-side bg-[url('./assets/images/bg.png')] ">
-                <AIbot />
-            </article>
+  const handleForgotPassword = async () => {
+    const response = await fetch(
+      "https://ai-doc-7h0i.onrender.com/api/v1/auth/forgot-password",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData.email),
+      }
+    );
+  };
 
+  return (
+    <section className="clash-variable flex gap-8 justify-items-center items-center overflow-y-hidden">
+      <article className="hidden md:block left-side bg-[url('./assets/images/bg.png')] ">
+        <AIbot />
+      </article>
 
-            <article className="w-full md:w-[30%] right-side m-10 md:m-20">
-                <div className="flex flex-col gap-5">
-                    <img src={Logo} className="w-8" />
-                    <div>
-                        <p className="text-2xl font-bold">Login to your account</p>
-                        <p>See what is going on with your health</p>
-                    </div>
-                </div>
+      <article className="w-full md:w-[30%] right-side m-10 md:m-20">
+        <div className="flex flex-col gap-5">
+          <img src={Logo} className="w-8" />
+          <div>
+            <p className="text-2xl font-bold">Login to your account</p>
+            <p>See what is going on with your health</p>
+          </div>
+        </div>
 
-                <div className="my-2">
-                    <div className="">
-                        <button className="text-center w-full my-4 rounded-md border border-color: inherit py-2 px-3">
-                            <span className="flex justify-content-center items-center">
-                                <img src={Google} className="pr-2" />
-                                Sign In with Google
-                            </span>
-                        </button>
+        <div className="my-2">
+          <div className="">
+            <button className="text-center w-full my-4 rounded-md border border-color: inherit py-2 px-3">
+              <span className="flex justify-content-center items-center">
+                <img src={Google} className="pr-2" />
+                Sign In with Google
+              </span>
+            </button>
+          </div>
+          <div className="text-center ">
+            <p className="opacity-50">
+              --------- or Sign in with email ---------
+            </p>
+          </div>
+        </div>
 
-                    </div>
-                    <div className="text-center ">
-                        <p className="opacity-50">
-                            --------- or Sign in with email ---------
-                        </p>
-                    </div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <br />
+          <input
+            type="email"
+            onChange={handleChange}
+            className="outline-none rounded-md w-full border border-color: inherit py-2 px-3 mb-8"
+            placeholder="johndoe@email.com"
+            name="email"
+          />
+          <br />
 
-                </div>
+          <label htmlFor="password">Password</label>
+          <br />
+          <input
+            type="password"
+            onChange={handleChange}
+            className="outline-none rounded-md w-full border border-color: inherit py-2 px-3 "
+            placeholder="***********"
+            name="password"
+          />
 
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="email">
-                        Email
-                    </label><br />
-                    <input type="email"
-                        onChange={handleChange}
-                        className="outline-none rounded-md w-full border border-color: inherit py-2 px-3 mb-8" placeholder="johndoe@email.com" name="email"
-                    />
-                    <br/>
+          <div className="flex justify-between items-center text-slate-600 text-sm mt-2">
+            <div>
+              <input type="checkbox" checked readOnly />
+              <span className="ml-1">Remember Me</span>
+            </div>
+            <p
+              onClick={handleForgotPassword}
+              className="hover:underline hover:cursor-pointer"
+            >
+              Forgot password?
+            </p>
+          </div>
 
-                    <label htmlFor="password">
-                        Password
-                    </label><br />
-                    <input type="password"
-                        onChange={handleChange}
-                        className="outline-none rounded-md w-full border border-color: inherit py-2 px-3 " placeholder="***********"
-                        name="password"
-                    />
+          <div className="flex mt-5">
+            <button
+              type="submit"
+              className="w-full justify-items-around rounded-md py-2 px-3 bg-[#0B63E5] text-white text-bold"
+            >
+              {isLoading ? "Loading..." : "Login"}
+            </button>
+          </div>
+        </form>
 
-                    <div className="flex justify-between items-center text-slate-600 text-sm mt-2">
-                        <div>
-                            <input type="checkbox" checked readOnly /><span className="ml-1">Remember Me</span>
-                        </div>
-                        <p
-                            onClick={handleForgotPassword}
-                            className="hover:underline hover:cursor-pointer"
-                        >
-                            Forgot password?
-                        </p>
-                    </div>
+        <div>
+          <p className="mt-5 text-center">
+            Not registered yet?
+            <Link to="/sign-up">
+              <span className="text-fuchsia-400 hover:underline ml-1">
+                Create an account
+              </span>
+            </Link>
+          </p>
+        </div>
 
-                    <div className="flex mt-5">
-                        <button
-                            type="submit"
-                            className="w-full justify-items-around rounded-md py-2 px-3 bg-[#0B63E5] text-white text-bold">
-                            {isLoading ? 'Loading...' : 'Login'}
-                        </button>
-                    </div>
-                </form>
-
-
-                <div>
-                    <p className="mt-5 text-center">
-                        Not registered yet?
-                        <Link to="/sign-up">
-                            <span className="text-fuchsia-400 hover:underline ml-1">
-                                Create an account
-                            </span>
-                        </Link>
-                    </p>
-                </div>
-
-                {error
-                    ?
-                    <p className="text-center text-red-500 text-sm mt-3">
-                        {error}
-                    </p>
-                    :
-                    ''
-                }
-            </article>
-        </section>
-    )
+        {error ? (
+          <p className="text-center text-red-500 text-sm mt-3">{error}</p>
+        ) : (
+          ""
+        )}
+      </article>
+    </section>
+  );
 }
 
-export default Login
-  
+export default Login;
