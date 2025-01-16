f411347a262f23bf20ef22abc2c25c7cd3cd161d
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-16 23:54:55 +0100

    Refactor App component for consistent code style and formatting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,32 @@
-import { MainView, Home, Chat, Signup, Login, Email, ForgotPassword } from "./pages";
-import { Routes, Route } from 'react-router-dom';
+import {
+  MainView,
+  Home,
+  Chat,
+  Signup,
+  Login,
+  Email,
+  ForgotPassword,
+} from "./pages";
+import { Routes, Route } from "react-router-dom";
 import { NavProvider } from "./context/NavContext.jsx";
 
 function App() {
-
   return (
     <div className="urbanist">
       <NavProvider>
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/sign-up' element={<Signup />} />
-          <Route path='/email-confirmation' element={<Email />} />
-          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/email-confirmation" element={<Email />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route element={<MainView />}>
-            <Route path="/home" element={<Home />}/>
-            <Route path='/chat' element={<Chat />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/chat" element={<Chat />} />
           </Route>
         </Routes>
       </NavProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
