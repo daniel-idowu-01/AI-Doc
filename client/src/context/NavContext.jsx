8adaec9fe9ce905224c1da8a5978848430656a0c
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-16 23:55:21 +0100

    Refactor NavContext for consistent code style and formatting

--- client/src/context/NavContext.jsx
+++ client/src/context/NavContext.jsx
@@ -1,18 +1,26 @@
 import { createContext, useState } from "react";
 
-const NavContext = createContext({})
+const NavContext = createContext({});
 
 export function NavProvider({ children }) {
-
-  const [query, setQuery] = useState([])
-  const [sideBar, setSideBar] = useState(false)
-  const [botResponse, setBotResponse] = useState([])
+  const [query, setQuery] = useState([]);
+  const [sideBar, setSideBar] = useState(false);
+  const [botResponse, setBotResponse] = useState([]);
 
   return (
-    <NavContext.Provider value={{sideBar, setSideBar, query, setQuery, botResponse, setBotResponse}}>
+    <NavContext.Provider
+      value={{
+        sideBar,
+        setSideBar,
+        query,
+        setQuery,
+        botResponse,
+        setBotResponse,
+      }}
+    >
       {children}
     </NavContext.Provider>
-  )
+  );
 }
 
 export default NavContext;
