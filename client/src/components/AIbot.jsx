1af08de89a524086e6a5e04f1892537ce02b530f
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-16 23:55:44 +0100

    Refactor Aibot component for consistent code style and improved readability

--- client/src/components/AIbot.jsx
+++ client/src/components/AIbot.jsx
@@ -1,22 +1,33 @@
 import React from "react";
-import { AiBot, Msg1, Msg2 } from '../assets/images';
-
+import { AiBot, Msg1, Msg2 } from "../assets/images";
 
 function Aibot() {
-    return(
-        <div className="m-12 flex flex-col justify-items-center items-center">
-            <div className="flex gap-6 justify-items-center items-center mb-10">
-                <img src={Msg1} alt="" className="" style={{ width: 180, height: 98 }}/>
-                <img src={AiBot} alt="" style={{ width: 200, height: 400 }} />
-                <img src={Msg2} alt="" className="mt-20" style={{ width: 180, height: 98 }}/>
-            </div>
+  return (
+    <div className="m-12 flex flex-col justify-items-center items-center">
+      <div className="flex gap-6 justify-items-center items-center mb-10">
+        <img
+          src={Msg1}
+          alt=""
+          className=""
+          style={{ width: 180, height: 98 }}
+        />
+        <img src={AiBot} alt="" style={{ width: 200, height: 400 }} />
+        <img
+          src={Msg2}
+          alt=""
+          className="mt-20"
+          style={{ width: 180, height: 98 }}
+        />
+      </div>
 
-            <div className="text-center">
-                <p className="font-bold text-3xl text-[#0B63E5]">Get Health Advise 24/7</p>
-                <p>Consult your favorite AI Doctor</p>
-            </div>
-        </div>
-    )
+      <div className="text-center">
+        <p className="font-bold text-3xl text-[#0B63E5]">
+          Get Health Advise 24/7
+        </p>
+        <p>Consult your favorite AI Doctor</p>
+      </div>
+    </div>
+  );
 }
 
-export default Aibot
+export default Aibot;
