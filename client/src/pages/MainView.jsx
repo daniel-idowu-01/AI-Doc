38e27fb3d3f429f1e13d91008e82df59d83a2fe9
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-16 23:54:30 +0100

    Refactor MainView component for consistent code style and formatting

--- client/src/pages/MainView.jsx
+++ client/src/pages/MainView.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { SideBar } from '../components'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { SideBar } from "../components";
+import { Outlet } from "react-router-dom";
 
 const MainView = () => {
   return (
-    <div className='flex'>
+    <div className="flex">
       <SideBar />
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default MainView
+export default MainView;
